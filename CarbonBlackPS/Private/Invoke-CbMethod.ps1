9efbe4f6aea65ae46e7f2da1544c0b278f228be4
Speedprior <44346321+Speedprior@users.noreply.github.com>
2022-10-07 07:38:59 -0400

    basic working config storage

--- CarbonBlackPS/Private/Invoke-CbMethod.ps1
+++ CarbonBlackPS/Private/Invoke-CbMethod.ps1
@@ -19,16 +19,25 @@ function Invoke-CbMethod {
     
     #Retrieve Configuration Values
     $config      = Get-CbConfig
+	write-host "The config: "
+	write-host ($config | convertto-json)
     $environment = $config.environment
-    $apiSecret   = $config.apiSecret
-    $apiID       = $config.apiID
+#    $apiSecret   = $config.apiSecret
+#    $apiID       = $config.apiID
     $orgKey      = $config.orgKey
-    $apiKey      = "$apiSecret/$apiID"
+#    $apiKey      = "$apiSecret/$apiID"
+    $apiKey = $config.apiKey
+	$customKey = $config.customKey
     $targetUrl   = $environment + $UriPreOrgKey + $orgKey + $UriPostOrgKey
-
+	
+	write-host ( $config | format-list)
+write-host "environment: " + $config.environment + $environment
+write-host "UriPreOrgKey: " + $URIpreOrgKey
+write-host "orgKey: " + $config.orgkey + $orgKey
+write-host "UriPostOrgKey: " + $UriPostOrgKey
     # Create headers
     $headers = @{
-        "X-Auth-Token" = $apiKey
+        "X-Auth-Token" = $customKey
         "Content-Type" = "application/json"
     }
 
@@ -39,19 +48,21 @@ function Invoke-CbMethod {
         Method      = $Method
         Headers     = $headers
     }
-
+    
     if ($Body) {
         $splatParameters.Add("Body", $Body)
     }
-
-    # Invoke the API
+	write-host ($splatParameters | convertto-json)
+# Invoke the API
+<#
     try {
         $webResponse = Invoke-RestMethod @splatParameters
     }
     catch {
-        throw "Error: Bad response received."
+        throw "Error: Bad response received: "
     }
-
+#>
+$webResponse = Invoke-RestMethod @splatParameters #temp, to get full error
     if ($webResponse) {
         return $webResponse
     }
